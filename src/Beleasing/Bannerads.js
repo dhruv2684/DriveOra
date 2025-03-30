b53976a8fafb8c97989956bb4d3c9f42faae3bd5
Dhruvv2023 <dholiyadhruv2023.katargam@gmail.com>
2025-03-30 17:25:05 +0530

    banner ads

--- src/Beleasing/Bannerads.js
+++ src/Beleasing/Bannerads.js
@@ -1,20 +1,33 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
-const Bannerads = ({ adId, scriptSrc }) => {
+const Bannerads = () => {
+    const [adId, setAdId] = useState("");
+
     useEffect(() => {
-        const script = document.createElement("script");
-        script.async = true;
-        script.setAttribute("data-cfasync", "false");
-        script.src = scriptSrc;
+        // Generate unique ID for the ad container
+        const uniqueId = "banner-ad-" + Math.random().toString(36).substr(2, 9);
+        setAdId(uniqueId);
 
-        document.body.appendChild(script);
+        if (adId) {
+            const script = document.createElement("script");
+            script.async = true;
+            script.dataset.cfasync = "false";
+            script.src = "//pl26253785.effectiveratecpm.com/e77bb2e2acba01d8566247f1d3ac8b8d/invoke.js";
 
-        return () => {
-            document.body.removeChild(script); // Cleanup script on unmount
-        };
-    }, [scriptSrc]);
+            document.body.appendChild(script);
 
-    return <div id={adId} style={{ margin: "20px 0" }}></div>;
+            return () => {
+                document.body.removeChild(script);
+            };
+        }
+    }, [adId]);
+
+    return (
+        <div>
+            <h2>Sponsored Ad</h2>
+            <div id={adId} style={{ width: "728px", height: "90px", margin: "20px auto", background: "#f1f1f1" }}></div>
+        </div>
+    );
 
 }
 

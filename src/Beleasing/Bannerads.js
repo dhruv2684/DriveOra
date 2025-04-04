a24d6b5a4f8e973f03d15c3ac13058da3918b5c4
Dhruvv2023 <dholiyadhruv2023.katargam@gmail.com>
2025-04-04 20:40:05 +0530

    mobile banner ad improve

--- src/Beleasing/Bannerads.js
+++ src/Beleasing/Bannerads.js
@@ -2,39 +2,34 @@ import { useEffect } from "react";
 
 const BannerAd = () => {
     useEffect(() => {
-        const adContainer = document.getElementById("ad-container");
+        const script1 = document.createElement("script");
+        script1.type = "text/javascript";
+        script1.innerHTML = `
+          atOptions = {
+            'key' : 'fbea65dd12ea97222d03b5fa3a287cd1',
+            'format' : 'iframe',
+            'height' : 50,
+            'width' : 320,
+            'params' : {}
+          };
+        `;
+        document.body.appendChild(script1);
 
-        if (adContainer) {
-            adContainer.innerHTML = ""; // Purge old ads
+        const script2 = document.createElement("script");
+        script2.type = "text/javascript";
+        script2.src = "//lanentablelanentableenemyattractive.com/fbea65dd12ea97222d03b5fa3a287cd1/invoke.js";
+        document.body.appendChild(script2);
 
-            const script1 = document.createElement("script");
-            script1.type = "text/javascript";
-            script1.innerHTML = `
-        atOptions = {
-          'key' : 'eaa60ae4631a88d3d82d16b72b9c9d81',
-          'format' : 'iframe',
-          'height' : 60,
-          'width' : 468,
-          'params' : {}
+        // Cleanup on component unmount
+        return () => {
+            document.body.removeChild(script1);
+            document.body.removeChild(script2);
         };
-      `;
-            adContainer.appendChild(script1);
-
-            const script2 = document.createElement("script");
-            script2.type = "text/javascript";
-            script2.src = "//www.highperformanceformat.com/eaa60ae4631a88d3d82d16b72b9c9d81/invoke.js";
-            script2.async = true;
-            script2.defer = true;
-            adContainer.appendChild(script2);
-        }
     }, []);
 
     return (
-        <div>
-            <div
-                id="ad-container"
-                style={{ width: "468px", height: "60px", overflow: "hidden" }}
-            ></div>
+        <div id="adsterra-banner" style={{ textAlign: "center", margin: "20px 0" }}>
+            {/* Ad will be injected here */}
         </div>
     );
 };
